ad863d67ba943c2f632a7ee310e40382ac5633a8
Server ÇETİN <servercetin@hotmail.com>
2022-07-26 20:46:30 +0300

    home page code

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,36 @@
+import LeftPhone from "./components/notLoggedIn/leftPhone/leftPhone";
+import RightForm from "./components/notLoggedIn/rightForm/rightForm";
+
 function App() {
-  return (
-      <h1 className="text-gray-900 bg-gray-700">insta clone</h1>
-  );
+
+
+    return (
+        <div className="h-full w-full  bg-main">
+
+            <div className="flex items-center justify-center gap-x-8 py-6">
+                <LeftPhone />
+                <RightForm />
+            </div>
+
+            <div className="container mx-auto justify-center items-center text-center">
+                <div>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                    <a className="text-gray-400 px-2 text-xs" href="#">Text</a>
+                </div>
+                <div>
+                    <p className="text-gray-400 px-2 text-xs">© 2022 Instagram from Meta</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ client/src/components/notLoggedIn/leftPhone/leftPhone.js
@@ -0,0 +1,44 @@
+import {useEffect, useRef} from "react";
+
+export default function LeftPhone(){
+    const ref = useRef()
+
+    useEffect(() => {
+        const images = ref.current.querySelectorAll("img")
+        const imgCount = images.length
+        let currentImage = 0
+        let lastImage = 0
+        let isItFirstTime = true
+
+        const imageTransition = () => {
+            !isItFirstTime && images[lastImage].classList.add('opacity-0')
+            images[currentImage].classList.remove('opacity-0')
+            lastImage = currentImage
+            currentImage = currentImage === imgCount - 1 ? 0 : currentImage += 1
+            isItFirstTime = false
+        }
+
+        imageTransition()
+        const interval = setInterval(imageTransition, 3000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [ref])
+
+    return(
+        <div className="hidden md:block text-center bg-[center_top_0.0rem] h-[581.15px] w-[380.31px] bg-[url('/src/assets/img/home-phone.png')]">
+            <div className="relative" ref={ref}>
+                <img className="absolute right-4 top-7 opacity-0 transition-opacity duration-1000 ease-in" alt=''
+                     src={require("../../../assets/img/phone1.png")}/>
+                <img className="absolute right-4 top-7 opacity-0 transition-opacity duration-1000 ease-in" alt=''
+                     src={require("../../../assets/img/phone2.png")}/>
+                <img className="absolute right-4 top-7 opacity-0 transition-opacity duration-1000 ease-in" alt=''
+                     src={require("../../../assets/img/phone3.png")}/>
+                <img className="absolute right-4 top-7 opacity-0 transition-opacity duration-1000 ease-in" alt=''
+                     src={require("../../../assets/img/phone4.png")}/>
+            </div>
+        </div>
+    )
+
+}
